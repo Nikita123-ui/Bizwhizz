5ac1998065b480386e48fae6cf5cc32b8e49b12f
sanat.gupta <sanat.gupta@galaxyweblinks.in>
2022-02-07 22:51:32 +0530

    its a blog

---
+++ js/slider-animation.js
@@ -0,0 +1,33 @@
+const leftArrow=document.querySelector('.left-arrow');
+const rightArrow=document.querySelector('.right-arrow');
+const slider=document.querySelector('.carousel__slider');
+
+var cardIndex;
+var slidenum;
+
+
+rightArrow.addEventListener('click', function(){
+	cardIndex=document.querySelector('input[name="slide"]:checked').value;
+	if(cardIndex<3){
+		cardIndex++;
+	}
+	else{
+		cardIndex=1;
+	}
+	slidenum='slide'+cardIndex;
+	document.getElementsById(slidenum).checked=true;
+});
+
+leftArrow.addEventListener('click', function(){
+	cardIndex=document.querySelector('input[name="slide"]:checked').value;
+	if (cardIndex>1) {
+	cardIndex--;
+	}
+	else{
+		cardIndex=3;
+	}
+	slidenum='slide'-cardIndex;
+	document.getElementsById(slidenum).checked=true;
+});
+
+
